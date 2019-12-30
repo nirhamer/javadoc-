9e1cc5121772e8e6523ae6fe242f3a4bb963e2fd
nir <nirhamer17@walla.co.il>
2019-12-30 10:09:31 +0200

    The first production task

--- src/main/java/ru/iitdgroup/nir/App.java
+++ src/main/java/ru/iitdgroup/nir/App.java
@@ -1,13 +1,131 @@
 package ru.iitdgroup.nir;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class App {
+
+    @Parameter(names = {"-r", "-recurse"}, description = "Recursively search path for java files")
+    private boolean recurse = false;
+
+    @Parameter(names = {"-f", "-regen"}, description = "Force regeneration")
+    private boolean regen = false;
+
+    @Parameter(description = "Path")
+    private String path;
+
+    public static void main(String[] args) {
+        App app = new App();
+        JCommander.newBuilder()
+                .addObject(app)
+                .build()
+                .parse(args);
+        try {
+            app.run();
+        } catch (IOException e) {
+            System.out.println("Unable to find path");
+        }
+    }
+
+    public void run() throws IOException {
+
+        if (path == null) {
+            System.out.println("A valid path must be supplied");
+            return;
+        }
+
+        List<Path> files =  recurse ?  searchRecursive(): searchNonRecursive();
+        for (Path path : files) {
+            generateJavaDoc(path);
+        }
+    }
+
+    public List<Path> searchNonRecursive() throws IOException {
+        return Files.list(Paths.get(path)).filter(f -> f.toString().endsWith(".java")).collect(Collectors.toList());
+    }
+
+    public List<Path> searchRecursive() throws IOException {
+        List<Path> result;
+        try (Stream<Path> walk = Files.walk(Paths.get(path))) {
+            result = walk.filter(f -> f.toString().endsWith(".java")).collect(Collectors.toList());
+        }
+        return result;
+    }
+
+    public void generateJavaDoc(Path path) throws IOException {
+        List<String> lines = Files.readAllLines(path);
+        if(containsJavaDoc(lines) && !regen){
+            return;
+        }
+
+        if(regen){
+            lines = removeJavaDoc(lines);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("/**");
+        boolean complete = false;
+        for(int i = 0; i < lines.size(); i++){
+            if(lines.get(i).contains("@DisplayName")){
+                sb.append("\n * ").append(extractFromAnnotationWithParameter(lines.get(i), "name"));
+            }
+            if(lines.get(i).contains("@Description")){
+                sb.append("\n * ").append(extractFromQuotes(lines.get(i)));
+                sb.append("\n**/");
+                complete = true;
+            }
+
+            if( complete && (lines.get(i).isEmpty() && !lines.get(i).startsWith("@"))){
+                lines.add(i, sb.toString());
+                i++;
+                sb.setLength(0);
+                sb.append("/**");
+                complete = false;
+            }
+        }
+        Files.write(path, lines);
+    }
+
+    public boolean containsJavaDoc(List<String> lines){
+        for (String line: lines) {
+            if(line.startsWith("/**")){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String extractFromAnnotationWithParameter(String line, String variableName){
+        final Pattern p = Pattern.compile(variableName + "[ \\t]*=[ \\t]*\"(.*?)\"");
+        final Matcher m = p.matcher(line);
+        if (m.find()) {
+            return m.group(1);
+        }
+        return null;
+    }
+
+    public static String extractFromQuotes(String line){
+        final Pattern p = Pattern.compile("\"(.*?)\"");
+        final Matcher m = p.matcher(line);
+        if (m.find()) {
+            return m.group(1);
+        }
+        return null;
+    }
+
+    public List<String> removeJavaDoc(List<String> lines) {
+        lines.removeIf(line -> line.startsWith("/**") || line.startsWith("*") || line.startsWith("**/"));
+        return lines;
     }
 }
--- src/test/java/ru/iitdgroup/nir/AppTest.java
+++ src/test/java/ru/iitdgroup/nir/AppTest.java
@@ -1,21 +1,45 @@
 package ru.iitdgroup.nir;
 
-import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertTrue;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+class App
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public static void main(String[] args) {
+
+        final File folder = new File("C:\\Users\\nir\\Desktop\\model");
+
+        List<String> result = new ArrayList<>();
+
+        search(".*\\.java", folder, result);
+
+        for (String s : result) {
+            System.out.println(s);
+        }
+
+    }
+
+    public static void search(final String pattern, final File folder, List<String> result) {
+        for (final File f : folder.listFiles()) {
+
+            if (f.isDirectory()) {
+                search(pattern, f, result);
+            }
+
+            if (f.isFile()) {
+                if (f.getName().matches(pattern)) {
+                    result.add(f.getAbsolutePath());
+
+
+                }
+            }
+        }
+
     }
 }
+
+
+
